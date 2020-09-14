7aeb4fb73d53bf86628e5960637d89d6a226ae0c
Mohammed Alzaid <m.al-zaid@live.com>
2020-09-15 02:47:46 +0300

    done

--- flights/views.py
+++ flights/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from datetime import datetime
 
 from .models import Flight, Booking
@@ -6,30 +6,38 @@ from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSeri
 
 
 class FlightsList(ListAPIView):
-	queryset = Flight.objects.all()
-	serializer_class = FlightSerializer
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
 
 
 class BookingsList(ListAPIView):
-	queryset = Booking.objects.filter(date__gte=datetime.today())
-	serializer_class = BookingSerializer
+    queryset = Booking.objects.filter(date__gte=datetime.today())
+    serializer_class = BookingSerializer
 
 
 class BookingDetails(RetrieveAPIView):
-	queryset = Booking.objects.all()
-	serializer_class = BookingDetailsSerializer
-	lookup_field = 'id'
-	lookup_url_kwarg = 'booking_id'
+    queryset = Booking.objects.all()
+    serializer_class = BookingDetailsSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'booking_id'
+
+
+class CreateBooking(CreateAPIView):
+    serializer_class = UpdateBookingSerializer
+
+    def perform_create(self, serializer):
+        flight = Flight.objects.get(id=self.kwargs.get("flight_id"))
+        serializer.save(user=self.request.user, flight=flight)
 
 
 class UpdateBooking(RetrieveUpdateAPIView):
-	queryset = Booking.objects.all()
-	serializer_class = UpdateBookingSerializer
-	lookup_field = 'id'
-	lookup_url_kwarg = 'booking_id'
+    queryset = Booking.objects.all()
+    serializer_class = UpdateBookingSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'booking_id'
 
 
 class CancelBooking(DestroyAPIView):
-	queryset = Booking.objects.all()
-	lookup_field = 'id'
-	lookup_url_kwarg = 'booking_id'
+    queryset = Booking.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'booking_id'
--- task_1/urls.py
+++ task_1/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from flights import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+
     path('admin/', admin.site.urls),
+
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
-    
-    path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
+    path('flights/<int:flight_id>/book/', views.CreateBooking.as_view(), name="book-flight"),
+
+    path('bookings/', views.BookingsList.as_view(), name="bookings-list"),
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
     path('booking/<int:booking_id>/update/', views.UpdateBooking.as_view(), name="update-booking"),
     path('booking/<int:booking_id>/cancel/', views.CancelBooking.as_view(), name="cancel-booking"),
